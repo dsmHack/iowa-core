31d3fe3d7df90c1c46851d53b68f8fc425a8c954
Tony Brobston <anthonybrobston@live.com>
2020-03-06 02:17:56 -0600

    Change prefix if gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-const isGhPages = process.env.NODE_ENV === 'gh-pages';
+const isGhPages = process.env.NODE_ENV === 'gh-pages' ;
+const prefix = isGhPages ? '/iowa-core' : ''
 
 module.exports = {
-  assetPrefix: isGhPages ? '/iowa-core' : '',
+  assetPrefix: prefix,
+  publicRuntimeConfig: {
+    linkPrefix: prefix,
+  },
   exportTrailingSlash: true,
   exportPathMap: function() {
     return {
