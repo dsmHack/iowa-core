2952741853964fa241b299e2538b91ee89e6f162
Tony Brobston <anthonybrobston@live.com>
2020-03-06 00:54:45 -0600

    Add extension if gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isGhPages = process.env.NODE_ENV === 'gh-pages';
+
 module.exports = {
+  assetPrefix: isGhPages ? '/iowa-core' : '',
   exportTrailingSlash: true,
   exportPathMap: function() {
     return {
