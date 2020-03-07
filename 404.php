682b3fdc5f5d68971ed30bbb366a12423522bd81
Val Yeltman <val.yeltman@workiva.com>
2020-03-06 22:26:26 -0600

    404 styles

--- 404.php
+++ 404.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<main role="main">
+    <!-- section -->
+    <section>
 
-			<!-- article -->
-			<article id="post-404">
+        <!-- article -->
+        <article id="post-404">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center"><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+                        <div class="not-found-btn">
+                            <a href="/>"><button class="btn btn-primary">Homepage</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <!-- /article -->
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+    </section>
+    <!-- /section -->
+</main>
 
 <?php get_footer(); ?>
