3a09e2b29e1b139beef07dc9f80dbc187404e51f
Tony Brobston <anthonybrobston@live.com>
2020-03-07 10:40:43 -0600

    Fixing ci

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 brew install git-ftp
+git ftp init -v --user $FTP_USERNAME --passwd $FTP_PASSWORD ftp://$FTP_HOSTNAME:$FTP_PORT/rethinkiowa.org/public_html/wp-content/themes/iowa-core
 git ftp push -v --user $FTP_USERNAME --passwd $FTP_PASSWORD ftp://$FTP_HOSTNAME:$FTP_PORT/rethinkiowa.org/public_html/wp-content/themes/iowa-core
