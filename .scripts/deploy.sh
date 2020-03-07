4301c4ccb52fb3aa8808abc5059c032a82ead313
Tony Brobston <anthonybrobston@live.com>
2020-03-06 20:07:01 -0600

    Fixing ci

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-apt-get update
-apt-get install git-ftp
 git ftp init --user $SFTP_USERNAME --passwd $SFTP_PASSWORD ftp://$SFTP_HOSTNAME:$SFTP_PORT/rethinkiowa.org/public_html/wp-content/themes/iowa-core
