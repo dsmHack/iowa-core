37e6caea72d396249d31402c78a04da444686916
Josh Angolano <jangolano@leantechniques.com>
2020-03-06 19:48:36 -0600

    Updating Map Php

--- map.php
+++ map.php
@@ -25,7 +25,145 @@
         }
 
         map.fitBounds(strictIowaBounds, 0);
+
+        loadMapShapes();
       }
+
+      function loadMapShapes() {
+          // load US state outline polygons from a GeoJson file
+          map.data.loadGeoJson('https://raw.githubusercontent.com/dsmHack/iowa-core/master/geojson/iowa_counties.json', {idPropertyName:'name'},
+          loadCensusData());
+      }
+
+      function loadCensusData() {
+    // load the requested variable from the census API (using local copies)
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://data.iowa.gov/resource/aeyn-twxp.json');
+    xhr.onload = function() {
+        var censusData = JSON.parse(xhr.responseText);
+        var mostRecentDate = censusData[0].month_ending;
+        censusData.shift(); // the first row contains column names
+        censusData.forEach(function(row) {
+            if(row.month_ending===mostRecentDate){
+                var censusVariable = parseFloat(row.benefits_paid);
+                var county_name = row.county_name;
+
+                // keep track of min and max values
+                if (censusVariable < censusMin) {
+                    censusMin = censusVariable;
+                }
+                if (censusVariable > censusMax) {
+                    censusMax = censusVariable;
+                }
+
+                let feature =  map.data.getFeatureById(county_name);
+                if(typeof feature != 'undefined'){
+                  
+                  // update the existing row with the new data
+                feature.setProperty('census_variable', censusVariable);
+                      map.data.setStyle(styleFeature);
+                  }
+
+            }   
+        });
+
+        // update and display the legend
+        document.getElementById('census-min').textContent =
+            censusMin.toLocaleString();
+        document.getElementById('census-max').textContent =
+            censusMax.toLocaleString();
+    };
+    xhr.send();
+    }
+
+      /** Removes census data from each shape on the map and resets the UI. */
+      function clearCensusData() {
+        censusMin = Number.MAX_VALUE;
+        censusMax = -Number.MAX_VALUE;
+        map.data.forEach(function(row) {
+          row.setProperty('census_variable', undefined);
+        });
+        document.getElementById('data-box').style.display = 'none';
+        document.getElementById('data-caret').style.display = 'none';
+      }
+
+      /**
+       * Applies a gradient style based on the 'census_variable' column.
+       * This is the callback passed to data.setStyle() and is called for each row in
+       * the data set.  Check out the docs for Data.StylingFunction.
+       *
+       * @param {google.maps.Data.Feature} feature
+       */
+      function styleFeature(feature) {
+        var low = [5, 69, 54];  // color of smallest datum
+        var high = [151, 83, 34];   // color of largest datum
+
+        // delta represents where the value sits between the min and max
+        var delta = (feature.getProperty('census_variable') - censusMin) /
+            (censusMax - censusMin);
+
+        var color = [];
+        for (var i = 0; i < 3; i++) {
+          // calculate an integer color based on the delta
+          color[i] = (high[i] - low[i]) * delta + low[i];
+        }
+
+        // determine whether to show this shape or not
+        var showRow = true;
+        if (feature.getProperty('census_variable') == null ||
+            isNaN(feature.getProperty('census_variable'))) {
+          showRow = false;
+        }
+        
+        
+        var outlineWeight = 0.5, zIndex = 1;
+        
+        /*if (feature.getProperty('state') === 'hover') {
+          outlineWeight = zIndex = 2;
+        }*/
+
+        return {
+          strokeWeight: outlineWeight,
+          strokeColor: '#fff',
+          zIndex: zIndex,
+          fillColor: 'hsl(' + color[0] + ',' + color[1] + '%,' + color[2] + '%)',
+          fillOpacity: 0.75,
+          visible: showRow
+        };
+      }
+
+      /**
+       * Responds to the mouse-in event on a map shape (state).
+       *
+       * @param {?google.maps.MouseEvent} e
+       */
+      function mouseInToRegion(e) {
+        // set the hover state so the setStyle function can change the border
+        e.feature.setProperty('state', 'hover');
+
+        var percent = (e.feature.getProperty('census_variable') - censusMin) /
+            (censusMax - censusMin) * 100;
+
+        // update the label
+        document.getElementById('data-label').textContent =
+            e.feature.getProperty('NAME');
+        document.getElementById('data-value').textContent =
+            e.feature.getProperty('census_variable').toLocaleString();
+        document.getElementById('data-box').style.display = 'block';
+        document.getElementById('data-caret').style.display = 'block';
+        document.getElementById('data-caret').style.paddingLeft = percent + '%';
+      }
+
+      /**
+       * Responds to the mouse-out event on a map shape (state).
+       *
+       * @param {?google.maps.MouseEvent} e
+       */
+      function mouseOutOfRegion(e) {
+        // reset the hover state, returning the border to normal
+        e.feature.setProperty('state', 'normal');
+      }
+
     </script>
     <!--Load the API from the specified URL
     * The async attribute allows the browser to render the page while the API loads
@@ -33,6 +171,6 @@
     * The callback parameter executes the initMap() function
     -->
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdgNID2pjCD3zbLeJHuuWEHd_UYBW-S94&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA7Kyc9KMvnH5Kq61zueqOy3-38Me81siw4&callback=initMap">
     </script>
 </h2>
