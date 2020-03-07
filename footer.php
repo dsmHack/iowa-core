7097a33d402e45abe8a77fc5ce0e1254ab5d80e8
Edwin <evalazquez@hy-vee.com>
2020-03-07 12:34:04 -0600

    adding footer content

--- footer.php
+++ footer.php
@@ -2,13 +2,20 @@
           <div class="container">
               <div class="row">
                 <div class="col text-center">
-                  <h2>About Rethink Iowa</h2>
-                  <p>
-                    Boilerplate about Rethink Iowa ipsum vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
-                  </p>
-                  <p>
-                    Copyright &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.rethinkiowa.com/">Rethink Iowa</a>
-                  </p>
+                    <h2>About Rethink Iowa</h2>
+                    <p>
+                        Boilerplate about Rethink Iowa ipsum vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
+                    </p>
+                    <h2>Contact Us</h2>
+                    <p>
+                        lorem@email.com
+                    </p>
+                    <p>
+                        123 Maint St. Des Moines, IA 50310
+                    </p>
+                    <p>
+                        Copyright &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.rethinkiowa.com/">Rethink Iowa</a>
+                    </p>
                 </div>
               </div>
           </div>
