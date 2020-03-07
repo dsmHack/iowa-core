6ef463d0525071f37934a1989c04c64b863264a1
Mike Kang <mike.kang@workiva.com>
2020-03-06 20:49:15 -0600

    remove post thumbnail and add relationship between story and community

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 if (function_exists('add_theme_support')) {
   add_theme_support('menus');
-  add_theme_support('post-thumbnails');
+//  add_theme_support('post-thumbnails');
   add_image_size('large', 700, '', true);
   add_image_size('medium', 250, '', true);
   add_image_size('small', 120, '', true);
