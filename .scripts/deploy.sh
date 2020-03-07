5ace86d60929b0869ef99d54e8f9e11c27eefaa8
Tony Brobston <anthonybrobston@live.com>
2020-03-06 20:36:01 -0600

    remove redundant git-ftp install

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install git-ftp
 git ftp init --user $FTP_USERNAME --passwd $FTP_PASSWORD ftp://$FTP_HOSTNAME:$FTP_PORT/rethinkiowa.org/public_html/wp-content/themes/iowa-core
