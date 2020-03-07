d553cadd5218c685599f09cfc08af70bb46a6c5b
Mike Kang <mike.kang@workiva.com>
2020-03-07 09:11:35 -0600

    map css

--- functions.php
+++ functions.php
@@ -29,6 +29,9 @@ function register_styles() {
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.min.css', array(), '1.0', 'all');
     wp_enqueue_style('normalize');
 
+    wp_register_style('map', get_template_directory_uri() . '/css/map.css', array(), '1.0', 'all');
+    wp_enqueue_style('map');
+
     wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
     wp_enqueue_style('bootstrap');
 
