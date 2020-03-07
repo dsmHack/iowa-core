8e1d9f27bd4f487a79477da6008d7c96ba9203a6
Tony Brobston <anthonybrobston@live.com>
2020-03-06 19:24:11 -0600

    Adding a deploy script

---
+++ .scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+apt-get update
+apt-get -qq install git-ftp
+git ftp init --user $SFTP_USERNAME --passwd $SFTP_PASSWORD ftp://$SFTP_HOSTNAME:$SFTP_PORT/rethinkiowa.org/public_html/wp-content/themes/iowa-core
