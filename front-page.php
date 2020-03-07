eb22d4ea73cf7e4f6c21faa74aab30c95a5370f4
Mike Kang <mike.kang@workiva.com>
2020-03-07 10:14:34 -0600

    story post styles

--- front-page.php
+++ front-page.php
@@ -4,20 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col">
+          <div class="title-block">
             <h1 class="text-center"><?php the_title();?></h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col map-summary">
-            <?php the_content();?>
+            <div class="col map-summary">
+              <?php the_content();?>
+            </div>
+          </div>
         </div>
     </div>
 </div>
+
 <div class="container-fluid">
   <?php include 'map.php';?>
 </div>
-
-
 
 <?php $community_query = new WP_Query(array(
   'post_type' => 'community',
@@ -28,12 +27,16 @@
     <div class="container">
       <?php while ($community_query->have_posts()) { ?>
         <?php $community_query->the_post();?>
-          <h2>
-            <a href="<?php the_permalink();?>">
-              <?php the_title();?>
-            </a>
-          </h2>
-          <hr />
+          <div class="row">
+            <div class="col">
+              <h2>
+                <a href="<?php the_permalink();?>">
+                  <?php the_title();?>
+                </a>
+              </h2>
+              <hr />
+            </div>
+          </div>
           <?php
             $stories = get_posts(array(
               'post_type' => 'story',
@@ -47,22 +50,28 @@
             ));
           ?>
           <?php if( $stories ): ?>
-            <ul class="row">
+            <div class="row">
             <?php foreach( $stories as $story): ?>
-                <li class="col-4">
-                    <a href="<?php echo get_permalink($story->ID);?>">
-                      <?php $image = get_field('teaser_photo',  $story->ID);?>
-                      <?php if(!empty($image)) { ?>
+                <article class="col-4">
+                  <a href="<?php echo get_permalink($story->ID);?>">
+                    <?php $image = get_field('teaser_photo',  $story->ID);?>
+                    <?php if(!empty($image)) { ?>
+                      <div class="image">
                         <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                      <?php } ?>
-                    </a>
+                      </div>
+                    <?php } ?>
+                  </a>
+                  <h3>
                     <a href="<?php echo get_permalink($story->ID);?>">
                       <?php echo get_the_title($story->ID);?>
                     </a>
+                  </h3>
+                  <p>
                     <?php echo get_the_excerpt($story->ID);?>
-                </li>
+                  </p>
+                </article>
             <?php endforeach; ?>
-            </ul>
+            </div>
           <?php endif; ?>
         <?php wp_reset_postdata();?>
       <?php } ?>
