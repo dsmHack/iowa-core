1e8d6413b8b4d94a93b46917a4ffbc7554f81510
Tony Brobston <anthonybrobston@live.com>
2020-03-06 21:04:48 -0600

    Fixing ci

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-git config git-ftp.url "$FTP_HOSTNAME"
-git config git-ftp.user "$FTP_USERNAME"
-git config git-ftp.password "$FTP_PASSWORD"
-git ftp catchup
+git ftp catchup --user $FTP_USERNAME --passwd $FTP_PASSWORD ftp://$FTP_HOSTNAME:$FTP_PORT/rethinkiowa.org/public_html/wp-content/themes/iowa-core
+travis_terminate 0
