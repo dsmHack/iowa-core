f7c1ca1de4d37272ca42fa16e4937146cf241521
Mike Kang <mike.kang@workiva.com>
2020-03-07 11:16:28 -0600

    single story frontend

--- single-story.php
+++ single-story.php
@@ -4,18 +4,37 @@
 <div class="container">
   <div class="row">
     <div class="col">
-
-    </div>
-  </div>
-</div>
-
-<div class="container">
-  <div class="row">
-    <div class="col">
       <div class="title-block">
         <h1 class="text-center">
           <?php the_title(); ?>
         </h1>
+      </div>
+      <div class="story-information-module">
+        <?php $story_date = the_field('date');?>
+        <?php if($story_date) { ?>
+          <h4>Date</h4>
+          <p><?php echo $story_date;?></p>
+        <?php } ?>
+
+        <?php if(have_rows('contributors')) { ?>
+          <h4>Contributors</h4>
+          <ul>
+            <?php while(have_rows('contributors')) { ?>
+              <li>
+                <?php the_row();?>
+                <?php echo get_sub_field('contributor_name');?>
+              </li>
+            <?php } ?>
+          </ul>
+        <?php } ?>
+        <h4>Minority Group</h4>
+        <p><?php the_field('minority_group');?></p>
+        <h4>Address</h4>
+        <p>
+          <?php the_field('street_address');?><br />
+          <?php the_field('city');?><br />
+          <?php the_field('zip');?>
+        </p>
       </div>
       <div class="content-block">
         <?php the_content();?>
@@ -24,52 +43,32 @@
   </div>
 </div>
 
-
-
-
-Date<br />
-<?php the_field('date');?>
-
-Contributors<br />
-<?php if(have_rows('contributors')) { ?>
-  <ul>
-    <?php while(have_rows('contributors')) { ?>
-      <li>
-        <?php the_row();?>
-        <?php echo get_sub_field('contributor_name');?>
-      </li>
-    <?php } ?>
-  </ul>
+<?php $images = get_field('image_gallery');?>
+  <?php if( $images ) { ?>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Gallery</h2>
+        </div>
+      </div>
+      <div class="row">
+        <?php foreach( $images as $image ){ ?>
+          <div class="col-md-4">
+            <a href="<?php echo esc_url($image['url']); ?>">
+               <img class="img-fluid" src="<?php echo esc_url($image['sizes']['full']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+            </a>
+            <p>
+              <?php echo esc_html($image['caption']); ?>
+            </p>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
 <?php } ?>
 
-Minority Group
-<?php the_field('minority_group');?>
+<div class="container">
 
-Street Address
-<?php the_field('street_address');?>
-
-City
-<?php the_field('city');?>
-
-Zip
-<?php the_field('zip');?>
-
-Image Gallery:
-<?php
-$images = get_field('image_gallery');
-if( $images ): ?>
-    <ul>
-        <?php foreach( $images as $image ): ?>
-            <li>
-                <a href="<?php echo esc_url($image['url']); ?>">
-                     <img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                </a>
-                <p><?php echo esc_html($image['caption']); ?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
-
+</div>
 Tags/Categories
 <?php
   $terms = wp_get_post_terms($post->ID);
@@ -84,9 +83,18 @@ Tags/Categories
 <?php $posts = get_field('community');?>
 <?php if($posts) { ?>
   <div class="community-module">
+    <h2>About the Community</h2>
     <?php foreach( $posts as $post){ ?>
       <?php setup_postdata($post);?>
       <article>
+        <a href="<?php echo get_permalink($story->ID);?>">
+          <?php $image = get_field('image',  $story->ID);?>
+          <?php if(!empty($image)) { ?>
+            <div class="image">
+              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+            </div>
+          <?php } ?>
+        </a>
         <h2>
           <a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
