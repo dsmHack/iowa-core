eb94e63edc9f26fd57ed72503e99422cc7d4f580
Mike Kang <mike.kang@workiva.com>
2020-03-07 13:16:47 -0600

    imagesloaded

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@
   $(function() {
     'use strict';
 
-    $('.grid').masonry({
+    var $grid = $('.grid').masonry({
       itemSelector: '.grid-item',
       columnWidth: '.grid-sizer',
       percentPosition: true,
     });
+
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
   });
 })(jQuery, this);
+
